6161a0d3562f471d6d1b10d9a40e9298200c0215
donato-fiore <50346119+donato-fiore@users.noreply.github.com>
2020-05-12 22:21:29 -0400

    updated yolo.py
    added color to the output log

--- yolo.py
+++ yolo.py
@@ -1,29 +1,35 @@
 from selenium import webdriver
 import time
 
-links = input("Enter yolo link: ")
-link = str(links)
-
+link = str(input("Enter yolo link: "))
+message = "0"
+error = "errors"
 print("[1] Bee Movie Script")
 print("[2] Never Gonna Give You Up")
 print("[3] Shrek Movie Script")
 print("[4] Africa Lyrics")
+print("[5] Rappin for Jesus")
 script = input("Choose Spam Method: ")
 
-driverpath="CHROMEDRIVER PATH"
+driverpath="/bin/chromedriver"
 driver = webdriver.Chrome(driverpath)
 driver.get(link)
-filler = ("filler")
-
 
 if script == "1":
     f = open('scripts/beemovie.txt', 'r')
+    type = "Bee Movie script"
 elif script == "2":
     f = open('scripts/nevergonna.txt', 'r')
+    type = "Never Gonna Give You Up lyrics"
 elif script == "3":
     print("No Shrek Script yet")
+    type = "Shrek script"
 elif script == "4":
     f = open('scripts/africa.txt', 'r')
+    type = "Africa Lyrics"
+elif script == "5":
+    f = open('scripts/jesus.txt', 'r')
+    type = "Rappin For Jesus"
 #add shrek script
     
 elem = driver.find_element_by_xpath('//*[@id="text"]')
@@ -34,9 +40,16 @@ for line in f:
         elem = driver.find_element_by_xpath('//*[@id="text"]')
         elem.send_keys(line)
         driver.find_element_by_xpath('//*[@id="send-button"]').click()
+        print("\033[1;34;40m" + "[+] Message sent: %s" % line + "\033[1;34;40m \n")
         time.sleep(2.5)
         driver.refresh()
-        print("[+] Message sent: %s" % line)
-    except TypeError:
-        print("[!] Message not sent: %s" % line)
-        break;      
+    except BaseException or WebDriverException:
+        print("\033[1;31;40m" + "[!] Message not sent: %s" % line + "\033[1;31;40m \n")
+        message = "1"
+        error = "error"
+        break;
+    
+if error == "0":
+    print("\033[0;37;46m" + type + " has finished with " + message + " " + error + "\033[0;37;46m \n")
+else:
+    print("\033[0;37;41m" + type + " has finished with " + message + " " + error + "\033[0;37;41m \n")
